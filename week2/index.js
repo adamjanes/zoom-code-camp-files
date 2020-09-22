01bcd3dcce490df29132f636e63c915bcb137e8e
Adam Janes <adam.michael.janes@gmail.com>
2020-09-22 08:52:41 +0100

    Finish 2.2

--- week2/index.js
+++ week2/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+function celsiusToFahrenheit(degreesInCelsius) {
+  const degreesInFahrenheit = (degreesInCelsius * 1.8) + 32
+  return degreesInFahrenheit
+}
+
+console.log(celsiusToFahrenheit(0)); // 32
+console.log(celsiusToFahrenheit(20)); // 68
+console.log(celsiusToFahrenheit(100)); // 212
