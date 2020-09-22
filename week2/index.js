8e1684b4953bf143443126cc895a4bcf97566acb
Adam Janes <adam.michael.janes@gmail.com>
2020-09-22 10:25:33 +0100

    Finish 2.3

--- week2/index.js
+++ week2/index.js
@@ -3,6 +3,14 @@ function celsiusToFahrenheit(degreesInCelsius) {
   return degreesInFahrenheit
 }
 
-console.log(celsiusToFahrenheit(0)); // 32
-console.log(celsiusToFahrenheit(20)); // 68
-console.log(celsiusToFahrenheit(100)); // 212
+// console.log(celsiusToFahrenheit(0)); // 32
+// console.log(celsiusToFahrenheit(20)); // 68
+// console.log(celsiusToFahrenheit(100)); // 212
+
+function getAgeFromUser() {
+  const age = prompt("How old are you at the moment?");
+  return age;
+}
+
+const userAge = getAgeFromUser();
+console.log(userAge);
