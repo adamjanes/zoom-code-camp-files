948408e1fc263ace050bb113bff4b0f90bb2102a
Adam Janes <adam.michael.janes@gmail.com>
2020-09-30 19:35:54 +0100

    Finish 3.5

--- week3/index.js
+++ week3/index.js
@@ -41,4 +41,70 @@ function buildPyramid() {
   }
 }
 
-buildPyramid();
+// buildPyramid();
+
+const data = [
+  {
+    name: "John Smith",
+    short_breath: false,
+    temperature: 37.2,
+    runny_nose: false,
+    itchy_eyes: true
+  },
+  {
+    name: "Jane Doe",
+    short_breath: true,
+    temperature: 38.6,
+    runny_nose: false,
+    itchy_eyes: false
+  },
+  {
+    name: "Jim Johnson",
+    short_breath: false,
+    temperature: 36.3,
+    runny_nose: true,
+    itchy_eyes: false
+  },
+  {
+    name: "Ginny Roberts",
+    short_breath: true,
+    temperature: 39.2,
+    runny_nose: false,
+    itchy_eyes: false
+  },
+  {
+    name: "Tina Chapman",
+    short_breath: false,
+    temperature: 36.4,
+    runny_nose: false,
+    itchy_eyes: false
+  },
+  {
+    name: "Dan Powers",
+    short_breath: true,
+    temperature: 38.0,
+    runny_nose: false,
+    itchy_eyes: true
+  },
+];
+
+function coronavirusScreening(clients) {
+  for (let i = 0; i < clients.length; i++) {
+    const client = clients[i];
+
+    if (client.temperature > 38 && client.short_breath) {
+      console.log(client.name + " might have coronavirus. We need to cancel their reservation.")
+    }
+    else if (client.temperature > 38 && !client.short_breath) {
+      console.log(client.name + " might have the flu. They need to wear a mask.")
+    }
+    else if (client.runny_nose && !client.itchy_eyes) {
+      console.log(client.name + " might have a cold. They need to wear a mask.")
+    }
+    else {
+      console.log(client.name + " looks healthy.")
+    }
+  }
+}
+
+coronavirusScreening(data); // should make logs
