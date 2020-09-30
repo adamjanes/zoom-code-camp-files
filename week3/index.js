dbdabd1ef498ecd3ebe91322bbde7593df4f004a
Adam Janes <adam.michael.janes@gmail.com>
2020-09-30 19:28:46 +0100

    Finish 3.3

--- week3/index.js
+++ week3/index.js
@@ -13,4 +13,22 @@ function frenchCalculator() {
   }
 }
 
-frenchCalculator();
+// frenchCalculator();
+
+for (let i = 0; i < 8; i++) {
+  let lineOutput = '';
+
+  // j here == "character position in line"
+  for (let j = 1; j <= 8; j++) {
+    // if we're not at the (8 - i)th position => space
+    if (j < 8 - i) {
+      lineOutput = lineOutput + " ";
+    }
+    // otherwise, it's a hash
+    else {
+      lineOutput = lineOutput + "#";
+    }
+  }
+
+  console.log(lineOutput);
+}
