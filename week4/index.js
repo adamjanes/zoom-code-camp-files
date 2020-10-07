5eb296ef2798c0db109e662a7d6d445fb47ad233
Adam Janes <adam.michael.janes@gmail.com>
2020-10-07 19:36:02 +0100

    Finish 4.4

--- week4/index.js
+++ week4/index.js
@@ -1,8 +1,38 @@
+function randomMove() {
+  // number between 1 and 3
+  const num = Math.floor(Math.random() * 3);
+
+  if (num === 1) {
+    return {
+      name: "rock",
+      src: "images/hand-rock.svg",
+      alt: "Rock",
+    };
+  } else if (num === 2) {
+    return {
+      name: "paper",
+      src: "images/hand-paper.svg",
+      alt: "Paper",
+    };
+  } else {
+    return {
+      name: "scissors",
+      src: "images/hand-scissors.svg",
+      alt: "Scissors",
+    };
+  }
+}
+
 const buttons = document.querySelectorAll(".button");
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function (event) {
     const playerMove = event.target.id;
-    console.log(playerMove) // "rock" or "paper" or "scissors"
+    const computerMove = randomMove();
+
+    // update computer's image to show move
+    const computerImage = document.getElementById("computer-image");
+    computerImage.src = computerMove.src;
+    computerImage.alt = computerMove.alt;
   });
 }
