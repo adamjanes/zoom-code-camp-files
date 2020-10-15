44ca592ed3dd07b805269d7df73c43764f5844bf
Adam Janes <adam.michael.janes@gmail.com>
2020-10-15 19:26:43 +0100

    Finish 5.4

--- week5/js/index.js
+++ week5/js/index.js
@@ -1,7 +1,32 @@
+function createCard(product) {
+  const card = document.createElement("div");
+  card.className = "product-card";
+  card.innerHTML = `
+    <div class="image-wrapper">
+      <img
+        class="product-image"
+        src="${product.image}"
+        alt="Picture of ${product.title}"
+      />
+    </div>
+    <div class="product-info">
+      <div class="product-price">$${product.price}</div>
+      <div class="product-title">
+        ${product.title}
+      </div>
+      <button class="product-button">Add to cart</button>
+    </div>
+  `;
+
+  document.getElementById("cards-container").appendChild(card);
+}
+
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data)
+    data.forEach((product) => {
+      createCard(product);
+    });
   })
   .catch((e) => {
     console.log(e);
