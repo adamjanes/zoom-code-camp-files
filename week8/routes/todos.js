e4f013aa189c62d623a79686ec8d8aaaa6c27e25
Adam Janes <adam.michael.janes@gmail.com>
2020-11-06 14:23:07 +0000

    Finish 8.5

--- week8/routes/todos.js
+++ week8/routes/todos.js
@@ -1,29 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const Todo = require("../models/Todo");
 
 // CREATE
-router.post("/", (req, res) => {
-  res.send("Creating new todo...");
+router.post("/", async (req, res) => {
+  if (!req.body.task) {
+    res.status(400).json({ message: `You must provide a task field to create a new TODO.`})
+  }
+  const todo = await Todo.create({
+    task: req.body.task,
+    completed: false
+  })
+  res.json(todo)
 });
 
 // READ all
-router.get("/", (req, res) => {
-  res.send("Retrieving all todos...");
+router.get("/", async (req, res) => {
+  const todos = await Todo.find()
+  res.json(todos)
 });
 
 // READ one /todos/123
-router.get("/:id", (req, res) => {
-  res.send("Retrieving one todo...");
+router.get("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id)
+    res.json(todo)
+  }
+  catch {
+    res.status(404).json({ message: `Couldn't find TODO with the ID ${req.params.id}`})
+  }
 });
 
 // UPDATE
-router.patch("/:id", (req, res) => {
-  res.send("Updating todo...");
+router.patch("/:id", async (req, res) => {
+  try {    
+    // filter req.body to only the fields that we want to update
+    const fieldsToUpdate = Object.keys(req.body).reduce((acc, curr) => {
+      // only allow us to update task or completed
+      if (curr === "task" || curr === "completed") {
+        return {
+          ...acc,
+          [curr]: req.body[curr] // [curr] is the name of the variable curr => "task" or "completed"
+        }
+      } else {
+        return acc
+      }
+    }, {})
+
+    const todo = await Todo.findByIdAndUpdate(req.params.id, { $set: { ...fieldsToUpdate } }, { new: true})
+    res.json(todo)
+  }
+  catch (error) {
+    res.status(404).json({ message: `Couldn't find TODO with the ID ${req.params.id}`})
+  }
 });
 
 // DESTROY
-router.delete("/:id", (req, res) => {
-  res.send("Deleting todo...");
+router.delete("/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndRemove(req.params.id)
+    res.json(todo)
+  }
+  catch {
+    res.status(404).json({ message: `Couldn't find TODO with the ID ${req.params.id}`})
+  }
 });
 
 module.exports = router;
